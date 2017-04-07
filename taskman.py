7a6a92eb8304fcb535e17d35e18cbfa8a3ea6504
AdriTech <dmkitui@gmail.com>
2017-04-07 13:58:40 +0300

    Added shelve files, functions

--- taskman.py
+++ taskman.py
@@ -11,24 +11,37 @@ def main(args):
     '''
 
     try:
-        with contextlib.closing(shelve.open(DATA, 'c')) as data:
+        with contextlib.closing(shelve.open(data, 'c')) as data:
             tasks = data['tasklist']
+            time = data['time']
+
+            tasknames = tasks.keys()
+
     except:
         print('No data present! Kinldy import your tasks first')
         return
 
-    if 
+    if
 
     if len(args) == 1 and args[0] == 'taskman.py' and args[1] == '-h':
         print('Usage: taskman.py taskname action\n\n'
               'Where taskname is the particular task.\n\n'
               '        Action is the command (status, or % complete)')
         return None
-    elif len(args) == 2 and args[1] is in :
+    elif len(args) == 2 and args[1] in tasknames :
+
+        taskevaluator(args)
+
         print('Usage Error.\n\n'
               'Usage: CurrentWeatherrr.py city\n'
               'Where city is the town you would like to get the current weather conditions.')
         return 'Help'
+
+def taskevaluator(args):
+
+
+
+
 
 
 if __name__ == "__main__":
