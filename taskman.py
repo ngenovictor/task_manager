bb71e45e3fe961d3f5a44626914d9ece7ebeb8ba
AdriTech <dmkitui@gmail.com>
2017-04-07 15:21:04 +0300

    Work in progress. Fixed user interface

--- taskman.py
+++ taskman.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sys
 import contextlib
+import shelve
 import datetime
 
 
@@ -13,15 +16,11 @@ def main(args):
     '''
 
     action = ['jobs', 'status', 'completed', 'partDone']
-    # try:
-    #     with contextlib.closing(shelve.open(data, 'c')) as data:
-    #         tasks = data['tasklist']
-    #         time = data['time']
-    #         tasknames = tasks.keys()
-    #
-    # except:
-    #     print('No data present! Kinldy import your tasks first')
-    #     return
+    # with contextlib.closing(shelve.open('data', 'c')) as data:
+    #     tasks = data['tasklist']
+    #     time = data['time']
+    #     tasknames = tasks.keys()
+
     tasks = {'Version Control': 0, 'Agile Methodology': 0, 'Programming Logic': 0}
     tasknames = tasks.keys()
 
@@ -29,7 +28,6 @@ def main(args):
         print('Usage: taskman.py taskname action\n\n'
               'Where taskname is the particular task.\n\n'
               '        Action is the command (status, or % complete)')
-        return None
     elif len(args) == 2 and args[1] in action:
         actions(tasks, args)
 
@@ -37,12 +35,15 @@ def main(args):
         taskevaluator(tasks, args)
 
 
-def taskevaluator(args):
+def taskevaluator(tasks, args):
     '''
     Function to evaluate the arguments an if valid execute!
     :param args:
     :return:
     '''
+    if args[2] == 'completed':
+        completed(tasks, args[1])
+
 def actions(tasks, args):
     '''
     function to commit tasks
@@ -53,22 +54,27 @@ def actions(tasks, args):
         print(tasks.keys())
 
     elif args[1] == 'status':
-        print(statusEvaluator(tasks))
-    elif args[2] == 'completed':
-        completed(tasks, args[1])
+        statusEvaluator(tasks)
 
 def statusEvaluator(tasks):
+    f = open('list.txt', 'r')
+    w = f.read()
+    print(w)
+    f.close
 
-    for value in tasks.values():
+    for value in w:
         i = 0
         if value == 1:
             i += 1
-    print('{} Tasks out {} Completed!'.format(i, len(tasks.values())
+    print('{} Tasks out {} Completed!'.format(i, len(tasks.values())))
+
 
 def completed(tasks, arg):
         tasks[arg] = 1
-        print(arg + 'Completed Successfully')
-        pass
+        f = open('list.txt', 'w')
+        f.write(str(tasks))
+        f.close
+        print(arg + ' Completed Successfully')
 
 
 
